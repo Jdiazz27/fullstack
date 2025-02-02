20a6c1c7b570c6b5d875fc0e66ed09c67359c1b0
Juan Diaz <juandiaz08@outlook.es>
2025-02-02 01:32:29 -0500

    falta js

---
+++ 2.HTMLCSS/todolist/script.js
@@ -0,0 +1,24 @@
+//js: lenguaje interpretado, dinamico, multiparadigma
+
+//variables y tipos
+var nombre = "Juan"
+var edad = 15
+var estatura = 1.80
+var estudiante = true
+
+//condicionales
+if (edad >= 18) {
+    console.log("Es mayor de edad")
+} else {
+    console.log("Es menor de edad")
+}
+
+//objetos
+let tarea = {
+    id: 1, 
+    nombre: "Tarea 1",
+  completada: false
+}
+console.log(tarea)
+console.log(tarea.nombre)
+
